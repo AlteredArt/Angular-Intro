4f4ece1f44207f59ed4b00fd4575d0c0017b35b0
Altered Art <cherokeedreemer@gmail.com>
2020-08-21 14:16:55 -0600

    pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component , ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +6,22 @@ import { Component } from '@angular/core';
   // template: '<h1>Hello Me</h1>',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
+ 
+  // interpolation
   title = 'Jared ';
   course = 'Angular7';
+  // boolean
+  flag = true;
+  // style binding
+  myclass ="FR";
+  color ="red";
+// event binding
+// hide : boolean = false;
+//   show(): void{
+//     console.log("hello from event binding")
+//   }
+//   showdata(): void{
+//     this.hide = !this.hide;
+//   }
 }
